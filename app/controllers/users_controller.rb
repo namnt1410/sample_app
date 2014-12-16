b259bc879a33a24fc104fa94aac51381a4083844
namnt1410 <namnt.1410@gmail.com>
2014-12-16 19:37:07 +0700

    Add password resets & email configuration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,13 +16,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.new(user_params)    # Not the final implementation!
-    if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
+  def create 
+     @user = User.new(user_params)
+    if @user.save 
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+    else 
       render 'new'
     end
   end
@@ -40,17 +40,6 @@ class UsersController < ApplicationController
 
   def new
    	@user = User.new
-  end
-
-  def create 
-  	 @user = User.new(user_params)
-  	if @user.save 
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
-  	else 
-  		render 'new'
-  	end
   end
 
   def edit
